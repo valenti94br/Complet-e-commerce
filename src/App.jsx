ac72f68f28d3918b8068bbc671b8942084e18b96
valenti94br <valenti94br@gmail.com>
2023-05-11 13:58:37 +0200

    login casi hecho

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import Home from './components/Home/Home';
 import Product from './components/Products/Product';
 import NavBar from './components/NavBar/NavBar';
 import { GlobalProvider } from "./context/GlobalState";
+import { UserProvider } from "./context/UserContext/UserState";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <UserProvider>
         <GlobalProvider>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product/>} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product" element={<Product/>} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Router>
         </GlobalProvider>
-      </Router>
+      </UserProvider>
     </div>
   );
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,37 +1,45 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+// Importamos las librerías necesarias
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../../context/UserContext/UserState';
 import NavBar from '../NavBar/NavBar';
 
+// Definimos el componente Login
 function Login() {
+  // Utilizamos el hook useContext para tener acceso al UserContext
+  const { login } = useContext(UserContext);
+
+  // Creamos estados locales para el email y la contraseña con los hooks useState
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  // Esta función se activa cada vez que el valor del campo de email cambia
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
 
+  // Esta función se activa cada vez que el valor del campo de contraseña cambia
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
+  // Esta función se activa cuando se envía el formulario
   const handleSubmit = async (event) => {
+    // Previene la recarga de la página al enviar el formulario
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8080/users/login', {
-        email: email,
-        password: password,
-      });
-      console.log(response.data);
-      // Handle successful login here
+      // Llama a la función login del UserContext pasando el email y la contraseña
+      await login({ email, password });
+      // Aquí manejaríamos el inicio de sesión exitoso
     } catch (error) {
       console.error(error);
-      // Handle login error here
+      // Aquí manejaríamos los errores en el inicio de sesión
     }
   };
 
+  // Renderizamos el formulario de inicio de sesión
   return (
     <div>
-      <h2>Login</h2>
+      <h2>Iniciar sesión</h2>
       <form onSubmit={handleSubmit}>
         <label>
           Email:
@@ -39,14 +47,15 @@ function Login() {
         </label>
         <br />
         <label>
-          Password:
+          Contraseña:
           <input type="password" value={password} onChange={handlePasswordChange} />
         </label>
         <br />
-        <button type="submit">Login</button>
+        <button type="submit">Iniciar sesión</button>
       </form>
     </div>
   );
 }
 
+// Exportamos el componente Login
 export default Login;
